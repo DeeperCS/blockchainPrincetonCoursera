1ce095a9a989ca7c48ed4c81de3adb52482c49a9
Catalina Ossa Orozco <catalina.ossa.orozco@ing.nl>
2017-07-27 14:57:06 +0200

    So far so good with TxHandlerOptimized

--- test/TxHandlerOptimized.java
+++ test/TxHandlerOptimized.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -9,20 +10,28 @@ import java.security.PublicKey;
 
 public class TxHandlerOptimized {
 
-    static PublicKey Cata;
-    static PublicKey Fra;
-
-    Transaction tx14, tx17, tx42, tx43;
+    static PublicKey Cata, Fra;
+    static Transaction tx14, tx17, tx42, tx43;
 
     @BeforeClass
     public static void setup() throws NoSuchProviderException, NoSuchAlgorithmException {
         SideTests st = new SideTests();
         Cata = st.generatePublicKey();
         Fra = st.generatePublicKey();
+
+        tx14 = new Transaction();
+        tx14.addOutput(2, Cata);
     }
 
     @Test
     public void testSetup(){
         System.out.println("all good");
+    }
+
+    @Test
+    public void isValidTxWithNoInputs(){
+        UTXOPool genesisUTXOPool = new UTXOPool();
+        TxHandler txHandler = new TxHandler(genesisUTXOPool);
+        Assert.assertTrue(txHandler.isValidTx(tx14));
     }
 }
